418b171de7000ed5945b70e34ba411129a1d7f7b
Gaby <gabygonzalezromero@protonmail.com>
2023-01-12 01:41:56 -0300

    fix(Mason.rc.lua): Se remueven LSP innesarios
    Se quitan LSP de TaildwindCSS
    
    Se quitan los LSP de TaildwindCSS por error en los servidores (?)

--- after/plugin/mason.rc.lua
+++ after/plugin/mason.rc.lua
@@ -6,28 +6,26 @@ local status2, lspconfig = pcall(require, 'mason-lspconfig')
 if (not status2) then return end
 
 mason.setup {
-  ui = {
-    icons = {
-      package_installed = "✓",
-      package_pending = "➜",
-      package_uninstalled = "✗"
-    }
-  },
-  lspconfig.setup {
-    ensure_installed = {
-      'html',
-      'cssls',
-      'taildwindcss',
-      'quick_lint_js',
-      'tsserver',
-      'emmet_ls',
-      'eslint',
-      'graphql',
-      'jsonls',
-      'sumneko_lua',
-      'slint_lsp',
+    ui = {
+        icons = {
+            package_installed = "✓",
+            package_pending = "➜",
+            package_uninstalled = "✗"
+        }
+    },
+    lspconfig.setup {
+        ensure_installed = {
+            'html',
+            'cssls',
+            'quick_lint_js',
+            'tsserver',
+            'emmet_ls',
+            'eslint',
+            'graphql',
+            'jsonls',
+            'sumneko_lua',
 
-    }
-  },
-  automatic_installation = true
+        }
+    },
+    automatic_installation = true
 }
